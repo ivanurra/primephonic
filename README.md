1af708ab1562ee78a85ccd0719e6be6ea6892456
Ivan Urra <ivan.urra.wd@gmail.com>
2022-10-19 07:24:10 +0200

    WIP

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 ### Primephonic
-
-14
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Primephonic</h1>
+      <main>
+        {/* Sidebar */}
+        {/* Center */}
+      </main>
+      <div>{/* Player */}</div>
+    </div>
+  );
+}
