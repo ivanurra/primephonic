a3085c892ab45dfbfa8361934988c6484def3170
Ivan Urra <ivan.urra.wd@gmail.com>
2023-04-20 19:09:30 +0200

    WIP

--- pages/auth/[...nextauth].js
+++ pages/auth/[...nextauth].js
@@ -2,6 +2,17 @@ import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 import { LOGIN_URL } from "../../lib/spotify";
 
+async function refreshAccessToken(token) {
+  try {
+  } catch (error) {
+    console.error(error);
+    return {
+      ...token,
+      error: "RefreshAccessToken",
+    };
+  }
+}
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -18,7 +29,25 @@ export const authOptions = {
   },
   callbacks: {
     async jwt({ token, account, user }) {
-      return
+      // Initial sign in
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpires: account.expires_at * 1000,
+        };
+      }
+
+      // Return previous token if the access token has not expired yet
+      if (Date.now() < token.accessTokenExpires) {
+        return token;
+      }
+
+      // Access token has expired, we need to refresh it
+      console.log("Access token has expired, refreshing");
+      return await refreshAccessToken(token);
     },
   },
 };
