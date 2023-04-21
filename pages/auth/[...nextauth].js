bc08d956ea94475614249002d198353e8484aa1a
Ivan Urra <ivan.urra.wd@gmail.com>
2023-04-21 08:31:38 +0200

    WIP

--- pages/auth/[...nextauth].js
+++ pages/auth/[...nextauth].js
@@ -1,14 +1,27 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
-import { LOGIN_URL } from "../../lib/spotify";
+import spotifyApi, { LOGIN_URL } from "../../lib/spotify";
 
 async function refreshAccessToken(token) {
   try {
+    spotifyApi.setAccessToken(token.accessToken);
+    spotifyApi.setRefreshToken(token.refreshToken);
+
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+    console.log("Refreshed token is ...", refreshedToken);
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now + refreshedToken.expires_in * 1000, // = 1h as 3600 returns from Spotify API
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+      // Replace if new one came back else back to old refresh token
+    };
   } catch (error) {
     console.error(error);
     return {
       ...token,
-      error: "RefreshAccessToken",
+      error: "RefreshAccessTokenError",
     };
   }
 }
@@ -42,6 +55,7 @@ export const authOptions = {
 
       // Return previous token if the access token has not expired yet
       if (Date.now() < token.accessTokenExpires) {
+        console.log("Existing access token is valid :)");
         return token;
       }
 
